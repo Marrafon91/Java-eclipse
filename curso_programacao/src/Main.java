8c8d8c32b28c517f72ebc23558f946361aa2d812
GuiM <Guilherme.Marrafon1@gmail.com>
2025-06-08 19:54:47 -0300

    Summary

---
+++ curso_programacao/src/Main.java
@@ -0,0 +1,25 @@
+
+public class Main {
+
+	public static void main(String[] args) {
+	
+	double b, B, h, area;
+		
+	b = 6.0;
+	B = 8.0;
+	h = 5.0;
+		
+	area = (b + B) / 2.0 * h;
+	
+	// CASTING
+	double a;
+	int c;
+	
+	a = 5.0;
+	c = (int) a;
+	
+	System.out.println(area);
+	System.out.println(c);
+	}
+
+}
