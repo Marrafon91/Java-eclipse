ad0e074bfefa33a5376b0c1306c168e7acf1fe12
GuiM <Guilherme.Marrafon1@gmail.com>
2025-06-09 10:46:17 -0300

    Summary

--- curso_programacao/src/tasks/Task004.java
+++ curso_programacao/src/tasks/Task004.java
@@ -1,10 +1,37 @@
 package tasks;
 
+import java.util.Locale;
+import java.util.Scanner;
+
 public class Task004 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+//	Fazer um programa que leia o número de um funcionário, seu número de horas trabalhadas, o valor que recebe por 
+//	hora e calcula o salário desse funcionário. A seguir, mostre o número e o salário do funcionário, com duas casas 
+//	decimais. 	
+		
+		Locale.setDefault(Locale.US);
+		Scanner sc = new Scanner(System.in);
+		
+		int funcionarios;
+		double horas;
+		double valor;
+		double valorTotal;
+		
+		funcionarios = sc.nextInt();
+		horas = sc.nextDouble();
+		valor = sc.nextDouble();
+		
+		valorTotal = horas * valor;
+		
+		System.out.println("Numero = " + funcionarios);
+		System.out.printf("Salario = %.2f%n ", valorTotal);
+		
+		
+		
+		
+		
+		sc.close();
 	}
 
 }
---
+++ curso_programacao/src/tasks/Task005.java
@@ -0,0 +1,38 @@
+package tasks;
+
+import java.util.Locale;
+import java.util.Scanner;
+
+public class Task005 {
+
+	public static void main(String[] args) {
+//	Fazer um programa para ler o código de uma peça 1, o número de peças 1, o valor unitário de cada peça 1, o 
+//	código de uma peça 2, o número de peças 2 e o valor unitário de cada peça 2. Calcule e mostre o valor a ser pago. 	
+		
+		Locale.setDefault(Locale.US);
+		Scanner sc = new Scanner(System.in);
+		
+		int codigo1, quantidade1,codigo2, quantidade2;
+		double preco1, preco2;
+		double resultado;
+		
+		codigo1 = sc.nextInt();
+		quantidade1 = sc.nextInt();
+		preco1 = sc.nextDouble();
+		
+		codigo2 = sc.nextInt();
+		quantidade2 = sc.nextInt();
+		preco2 = sc.nextDouble();
+		
+		resultado = quantidade1 * preco1 + quantidade2 * preco2;
+		
+		System.out.printf("Valor a Pagar: %.2f%n", resultado);
+		
+		
+		
+		
+		
+		sc.close();
+	}
+
+}
