0ffba5e71a36b150d25f70c276813b283a82e61c
GuiM <Guilherme.Marrafon1@gmail.com>
2025-06-09 12:04:31 -0300

    Task008

---
+++ curso_programacao/src/tasks/Task008.java
@@ -0,0 +1,30 @@
+package tasks;
+
+import java.util.Scanner;
+
+public class Task008 {
+
+	public static void main(String[] args) {
+// Fazer um programa para ler um número inteiro e dizer se
+// este número é par ou ímpar.
+		
+		Scanner sc = new Scanner(System.in);
+		
+		int N;
+		
+		N = sc.nextInt();
+		
+		if(N  %2 == 0) {
+			System.out.println("Numero PAR!");
+		} else {
+			System.out.println("Numero IMPAR!");
+		}
+		
+		
+		
+		
+		sc.close();
+
+	}
+
+}
