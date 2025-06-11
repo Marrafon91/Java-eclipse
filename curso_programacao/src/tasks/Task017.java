eecc1bba6ad2de4d3a70856e89a1d2fe7fcdc980
GuiM <Guilherme.Marrafon1@gmail.com>
2025-06-11 12:21:58 -0300

    Task017

--- curso_programacao/src/tasks/Task017.java
+++ curso_programacao/src/tasks/Task017.java
@@ -1,10 +1,45 @@
 package tasks;
 
+import java.util.Locale;
+import java.util.Scanner;
+
 public class Task017 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		 
+//		Um Posto de combustíveis deseja determinar qual de seus produtos tem a preferência de seus clientes. Escreva 
+//		um algoritmo para ler o tipo de combustível abastecido (codificado da seguinte forma: 1.Álcool 2.Gasolina 3.Diesel 
+//		4.Fim). Caso o usuário informe um código inválido (fora da faixa de 1 a 4) deve ser solicitado um novo código (até 
+//		que seja válido). O programa será encerrado quando o código informado for o número 4. Deve ser escrito a 
+//		mensagem: "MUITO OBRIGADO" e a quantidade de clientes que abasteceram cada tipo de combustível, conforme 
+//		exemplo.
+		
+		Locale.setDefault(Locale.US);
+		Scanner sc = new Scanner(System.in);
+		
+		int alcool = 0;
+		int gasolina = 0;
+		int diesel = 0;
+		int x = sc.nextInt();
+		
+		while(x != 4) {
+			if(x == 1) {
+				alcool +=  1;
+			} else if(x == 2) {
+				gasolina += 1;
+			} else if(x == 3) {
+				diesel += 1;
+			}
+			x = sc.nextInt();
+			System.out.println("Muito Obrigado!!!");
+			System.out.println("Alccol: " + alcool);
+			System.out.println("Gasolina: " + gasolina);
+			System.out.println("Diesel " + diesel);
+			
+		}
+		
+		
+		sc.close();
 	}
 
 }
