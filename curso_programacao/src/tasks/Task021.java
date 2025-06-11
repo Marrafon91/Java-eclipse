29ef5d0888bc158ca03d6fb8a9cdaacfaac2486f
GuiM <Guilherme.Marrafon1@gmail.com>
2025-06-11 17:06:52 -0300

    Task021

--- curso_programacao/src/tasks/Task021.java
+++ curso_programacao/src/tasks/Task021.java
@@ -8,23 +8,23 @@ public class Task021 {
 	public static void main(String[] args) {
 
 //	Fazer um programa para ler um número N. Depois leia N pares de números e mostre a divisão do primeiro pelo 
-//  	segundo. Se o denominador for igual a zero, mostrar a mensagem "divisao impossivel". 
-		
+//  segundo. Se o denominador for igual a zero, mostrar a mensagem "divisao impossivel". 
+
 		Locale.setDefault(Locale.US);
 		Scanner sc = new Scanner(System.in);
-		
+
 		int n = sc.nextInt();
-		
-		for(int i = 0;i < n; i++) {
+
+		for (int i = 0; i < n; i++) {
 			double num1 = sc.nextDouble();
 			double num2 = sc.nextDouble();
 			double resultado = num1 / num2;
-			if(resultado == 0) {
+			if (resultado == 0) {
 				System.out.println("Divisão impossivel");
 			}
 			System.out.printf("%.1f%n", resultado);
 		}
-		
+
 		sc.close();
 	}
 
