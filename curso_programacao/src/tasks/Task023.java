d4dbfc98fb3e0758ec010e6b545feff8dbdcb718
GuiM <Guilherme.Marrafon1@gmail.com>
2025-06-11 17:52:15 -0300

    Task023

---
+++ curso_programacao/src/tasks/Task023.java
@@ -0,0 +1,26 @@
+package tasks;
+
+import java.util.Locale;
+import java.util.Scanner;
+
+public class Task023 {
+
+	public static void main(String[] args) {
+//	Ler um número inteiro N e calcular todos os seus divisores.
+		Locale.setDefault(Locale.US);
+		Scanner sc = new Scanner(System.in);
+		
+		int n = sc.nextInt();
+		
+		for(int i = 1; i <= n; i++) {
+			if(n % i == 0) {
+				System.out.println("Seus divisores = " + i);
+			}
+		}
+		
+		
+		
+		sc.close();
+	}
+
+}
