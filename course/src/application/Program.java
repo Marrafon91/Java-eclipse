508726f198c63eedf3f67e52a31728de1797b2c2
GuiM <Guilherme.Marrafon1@gmail.com>
2025-06-25 15:15:35 -0300

    Summary

--- course/src/application/Program.java
+++ course/src/application/Program.java
@@ -10,7 +10,7 @@ public class Program {
 	public static void main(String[] args) {
 		Locale.setDefault(Locale.US);
 		Scanner sc = new Scanner(System.in);
-		
+
 		Product product = new Product();
 		System.out.println("Enter product data: ");
 		System.out.print("Name: ");
@@ -19,26 +19,26 @@ public class Program {
 		product.price = sc.nextDouble();
 		System.out.print("Quantity in stock: ");
 		product.quantity = sc.nextInt();
-		
+
 		System.out.println();
 		System.out.println("Product data: " + product);
-		
+
 		System.out.println();
 		System.out.print("Enter the number of products to be added in stock: ");
 		int quantity = sc.nextInt();
 		product.addProducts(quantity);
-		
+
 		System.out.println();
 		System.out.println("Updated data: " + product);
-		
+
 		System.out.println();
 		System.out.print("Enter the number of products to be removed from stock: ");
 		int removed = sc.nextInt();
 		product.removeProducts(removed);
-		
+
 		System.out.println();
 		System.out.println("Removed data: " + product);
-		
+
 		sc.close();
 	}
 
