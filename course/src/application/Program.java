ea70e98d270afd8c501b2f41f88728105e907a9d
GuiM <Guilherme.Marrafon1@gmail.com>
2025-06-25 12:44:40 -0300

    Update program.java

--- course/src/application/Program.java
+++ course/src/application/Program.java
@@ -20,7 +20,24 @@ public class Program {
 		System.out.print("Quantity in stock: ");
 		product.quantity = sc.nextInt();
 		
-		System.out.println(product.toString());
+		System.out.println();
+		System.out.println("Product data: " + product);
+		
+		System.out.println();
+		System.out.print("Enter the number of products to be added in stock: ");
+		int quantity = sc.nextInt();
+		product.addProducts(quantity);
+		
+		System.out.println();
+		System.out.println("Updated data: " + product);
+		
+		System.out.println();
+		System.out.print("Enter the number of products to be removed from stock: ");
+		int removed = sc.nextInt();
+		product.removeProducts(removed);
+		
+		System.out.println();
+		System.out.println("Removed data: " + product);
 		
 		sc.close();
 	}
