4c448416a9530cfad9f38ad26c32cb08c6b23011
GuiM <Guilherme.Marrafon1@gmail.com>
2025-06-26 20:45:29 -0300

    Summary

--- curso_programacao/src/tasks/task003.java
+++ curso_programacao/src/tasks/task003.java
@@ -10,19 +10,19 @@ public class Task003 {
 //		A, B, C e D. A seguir, calcule e mostre a diferença
 //		do produto de A e B pelo produto de C e D segundo a
 //		 fórmula: DIFERENCA = (A * B - C * D). 
-		
+
 		Locale.setDefault(Locale.US);
 		Scanner sc = new Scanner(System.in);
-		
+
 		int A, B, C, D, diferenca;
-		 A = sc.nextInt();
-		 B = sc.nextInt();
-		 C = sc.nextInt();
-		 D = sc.nextInt();
-		
+		A = sc.nextInt();
+		B = sc.nextInt();
+		C = sc.nextInt();
+		D = sc.nextInt();
+
 		diferenca = A * B - C * D;
-		
-		System.out.println("Diferença = " + diferenca );
+
+		System.out.println("Diferença = " + diferenca);
 
 	}
 
