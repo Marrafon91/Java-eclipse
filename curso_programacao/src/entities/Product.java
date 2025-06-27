7444da7dbf1540c9f8ff52e2dbce2dcff44fb2e0
GuiM <Guilherme.Marrafon1@gmail.com>
2025-06-27 08:16:07 -0300

    Update Product

--- curso_programacao/src/entities/Product.java
+++ curso_programacao/src/entities/Product.java
@@ -2,13 +2,13 @@ package entities;
 
 public class Product {
 
-	private String name;
+	String name;
 	private double price;
 	private int quantity;
 	
 	public Product() {
-		
 	}
+	
 	public Product(String name, double price, int quantity) {
 		this.name = name;
 		this.price = price;
@@ -17,29 +17,28 @@ public class Product {
 	public Product(String name, double price) {
 		this.name = name;
 		this.price = price;
-		
-	} // Sobre carga
+	}
 	
 	public String getName() {
 		return name;
 	}
-	
+
 	public void setName(String name) {
 		this.name = name;
 	}
-	
+
 	public double getPrice() {
 		return price;
 	}
-	
+
 	public void setPrice(double price) {
 		this.price = price;
 	}
-	
+
 	public int getQuantity() {
 		return quantity;
-	}
-	
+	}	
+
 	public double totalValueInStock() {
 		return price * quantity;
 	}
