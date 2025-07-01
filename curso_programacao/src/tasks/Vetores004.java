d3936ec7a806e83414efd7ccc83a7a3d89b9be34
GuiM <Guilherme.Marrafon1@gmail.com>
2025-07-01 08:55:19 -0300

    Vetores004

---
+++ curso_programacao/src/tasks/Vetores004.java
@@ -0,0 +1,38 @@
+package tasks;
+
+import java.util.Locale;
+import java.util.Scanner;
+
+public class Vetores004 {
+
+	public static void main(String[] args) {
+		Locale.setDefault(Locale.US);
+		Scanner sc = new Scanner(System.in);
+
+		System.out.print("Quantos numeros voce vai digitar ? ");
+
+		int N = sc.nextInt();
+		int[] vet = new int[N];
+
+		for (int i = 0; i < vet.length; i++) {
+			System.out.print("Digite um numero: ");
+			vet[i] = sc.nextInt();
+		}
+
+		System.out.println("\nNUMEROS PARES:");
+
+		int pares = 0;
+		for (int i = 0; i < vet.length; i++) {
+			if (vet[i] % 2 == 0) {
+				System.out.printf("%d  ", vet[i]);
+				pares++;
+			}
+		}
+		System.out.println();
+		System.out.print("\nQUANTIDADE DE  PARES =  " + pares);
+
+		sc.close();
+
+	}
+
+}
