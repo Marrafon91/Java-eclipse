8e84d2c0e8e977798edf038452f40c7ea501e7c6
GuiM <Guilherme.Marrafon1@gmail.com>
2025-07-03 08:58:23 -0300

    Update Program

--- exercicio_OO_list/src/application/Program.java
+++ exercicio_OO_list/src/application/Program.java
@@ -38,11 +38,19 @@ public class Program {
 
 		System.out.println("Enter the employee id that will have salary increase :");
 		int idslary = sc.nextInt();
+		Integer pos = position(list, idslary);
+		if(pos == null) {
+			System.out.println("This id does not exist!");
+		} else {
+			System.out.print("Enter the percentage: ");
+			double percent = sc.nextDouble();
+			list.get(pos).increaseSalary(percent);
+		}
 
 		sc.close();
 	}
 
-	public Integer position(List<Employee> list, int id) {
+	public static Integer position(List<Employee> list, int id) {
 		for (int i = 0; i < list.size(); i++) {
 			if (list.get(i).getId() == id) {
 				return i;
