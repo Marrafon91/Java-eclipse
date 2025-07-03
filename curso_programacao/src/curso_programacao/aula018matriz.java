96bc8f926a02568a4932a0d5ae1335f74d884ab0
GuiM <Guilherme.Marrafon1@gmail.com>
2025-07-03 15:27:33 -0300

    aula018matriz

---
+++ curso_programacao/src/curso_programacao/aula018matriz.java
@@ -0,0 +1,36 @@
+package curso_programacao;
+
+import java.util.Locale;
+import java.util.Scanner;
+
+public class aula018matriz {
+
+	public static void main(String[] args) {
+		Locale.setDefault(Locale.US);
+		Scanner sc = new Scanner(System.in);
+
+		System.out.print("Type a number: ");
+		int N = sc.nextInt();
+		int[][] mat = new int[N][N];
+
+		int numerosNegativos = 0;
+		for (int i = 0; i < mat.length; i++) {
+			for (int j = 0; j < mat[i].length; j++) {
+				mat[i][j] = sc.nextInt();
+				if (mat[i][j] < 0) {
+					numerosNegativos++;
+				}
+			}
+		}
+
+		System.out.println("Main diagonal: ");
+		for (int i = 0; i < mat.length; i++) {
+			System.out.print(mat[i][i] + " ");
+		}
+		System.out.println();
+		System.out.print("Negative numbers = " + numerosNegativos);
+
+		sc.close();
+	}
+
+}
