cbf1a924b198ff93cd3ff83fdff7391a51facc63
GuiM <Guilherme.Marrafon1@gmail.com>
2025-07-06 19:22:15 -0300

    Matriz001

--- curso_programacao/src/tasks/Matriz001.java
+++ curso_programacao/src/tasks/Matriz001.java
@@ -9,10 +9,9 @@ public class Matriz001 {
 		Locale.setDefault(Locale.US);
 		Scanner sc = new Scanner(System.in);
 
-		System.out.print("Digite quantas linhas depois colunas: ");
-		int m = sc.nextInt();
-		int n = sc.nextInt();
-		int[][] mat = new int[m][n];
+		int M = sc.nextInt();
+		int N = sc.nextInt();
+		int[][] mat = new int[M][N];
 
 		for (int i = 0; i < mat.length; i++) {
 			for (int j = 0; j < mat[i].length; j++) {
@@ -20,28 +19,16 @@ public class Matriz001 {
 			}
 		}
 
-		int x = sc.nextInt();
-
+		System.out.println("VALORES NEGATIVO: ");
 		for (int i = 0; i < mat.length; i++) {
 			for (int j = 0; j < mat[i].length; j++) {
-				if (mat[i][j] == x) {
-					System.out.println("Position linha: " + i + " coluna: " + j);
-					if (j > 0) {
-						System.out.println("Left: " + mat[i][j - 1]);
-					}
-					if (i > 0) {
-						System.out.println("Up: " + mat[i - 1][j]);
-					}
-					if (j < mat[i].length - 1) {
-						System.out.println("Right: " + mat[i][j + 1]);
-					}
-					if (i < mat.length - 1) {
-						System.out.println("Down: " + mat[i + 1][j]);
-					}
+				if (mat[i][j] < 0) {
+					System.out.println(mat[i][j]);
 				}
 			}
 		}
 
 		sc.close();
 	}
+
 }
