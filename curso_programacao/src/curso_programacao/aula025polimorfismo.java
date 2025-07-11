570a9f4720ad2c290d735716f364ec0c88de4280
GuiM <Guilherme.Marrafon1@gmail.com>
2025-07-11 13:49:37 -0300

    Heranca

---
+++ curso_programacao/src/curso_programacao/aula025polimorfismo.java
@@ -0,0 +1,22 @@
+package curso_programacao;
+
+import java.util.Locale;
+import java.util.Scanner;
+
+import entity.BusinessAccount;
+
+public class aula025polimorfismo {
+
+	public static void main(String[] args) {
+		Locale.setDefault(Locale.US);
+		Scanner sc = new Scanner(System.in);
+		
+		BusinessAccount account = new BusinessAccount();
+		
+		account.get
+		
+		
+		sc.close();
+	}
+
+}
