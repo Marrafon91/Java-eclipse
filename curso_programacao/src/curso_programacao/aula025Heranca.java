4262c5224ee290ec61cbd550c1c71112e7c6cd83
GuiM <Guilherme.Marrafon1@gmail.com>
2025-07-11 14:48:34 -0300

    Summary

--- curso_programacao/src/curso_programacao/aula025Heranca.java
+++ curso_programacao/src/curso_programacao/aula025Heranca.java
@@ -2,16 +2,29 @@ package curso_programacao;
 
 import java.util.Locale;
 import java.util.Scanner;
+
+import entity.Account;
 import entity.BusinessAccount;
+import entity.SavingsAccount;
 
 public class aula025Heranca {
 
 	public static void main(String[] args) {
 		Locale.setDefault(Locale.US);
 		Scanner sc = new Scanner(System.in);
+
+		Account acc1 = new Account(1001, "Guilherme", 1000.0);
+		acc1.withDraw(200);
+		System.out.println(acc1.getBalance());
+
+		Account acc2 = new SavingsAccount(1002, "Maria", 1000.0, 0.01);
+		acc2.withDraw(200);
+		System.out.println(acc2.getBalance());
 		
-		BusinessAccount account = new BusinessAccount();
-				
+		Account acc3 = new BusinessAccount(1003, "Alex", 1000.0, 500.0);
+		acc3.withDraw(200);
+		System.out.println(acc3.getBalance());
+		
 		sc.close();
 	}
 
--- curso_programacao/src/entity/SavingsAccount.java
+++ curso_programacao/src/entity/SavingsAccount.java
@@ -25,4 +25,9 @@ public class SavingsAccount extends Account {
 		balance += balance * interestRate;
 	}
 	
+	@Override
+	public void withDraw(double amount) {
+		balance -= amount;
+	}
+	
 }
