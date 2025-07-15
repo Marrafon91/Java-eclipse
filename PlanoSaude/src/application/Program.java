5f074ec2c8cad4b2bc3e33949fb633fc3d9f36eb
GuiM <Guilherme.Marrafon1@gmail.com>
2025-07-15 19:51:07 -0300

    Update

--- PlanoSaude/src/application/Program.java
+++ PlanoSaude/src/application/Program.java
@@ -1,17 +1,25 @@
 package application;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Locale;
 import java.util.Scanner;
+
+import entities.Company;
+import entities.Individual;
+import entities.TaxPayer;
 
 public class Program {
 
 	public static void main(String[] args) {
 		Locale.setDefault(Locale.US);
 		Scanner sc = new Scanner(System.in);
-		
+
+		List<TaxPayer> list = new ArrayList<>();
+
 		System.out.print("Enter the number od tax payers: ");
 		int n = sc.nextInt();
-		
+
 		for (int i = 1; i <= n; i++) {
 			System.out.print("Tax payer #" + i + " data:");
 			System.out.print("individual or company (i/c)? ");
@@ -21,11 +29,24 @@ public class Program {
 			String name = sc.nextLine();
 			System.out.print("Anual income: ");
 			double income = sc.nextDouble();
-			if(ch == 'i' || ch == 'I') {
-				
+			if (ch == 'i' || ch == 'I') {
+				System.out.print("Health expenditures: ");
+				double healthExpenditures = sc.nextDouble();
+				list.add(new Individual(name, income, healthExpenditures));
+			} else if (ch == 'c' || ch == 'C') {
+				System.out.print("Number of employees: ");
+				int numberOfEmployees = sc.nextInt();
+				list.add(new Company(name, income, numberOfEmployees));
 			}
 		}
-		
+
+		System.out.println();
+		System.out.println("TAXES PAID");
+
+		for (TaxPayer taxes : list) {
+			System.out.print(taxes.tax());
+		}
+
 		sc.close();
 	}
 
