8d47575b9a25eaad565f2d8508b5be939ee1b766
GuiM <Guilherme.Marrafon1@gmail.com>
2025-07-15 20:25:33 -0300

    Update Program

--- PlanoSaude/src/application/Program.java
+++ PlanoSaude/src/application/Program.java
@@ -18,35 +18,41 @@ public class Program {
 		List<TaxPayer> list = new ArrayList<>();
 
 		System.out.print("Enter the number od tax payers: ");
-		int n = sc.nextInt();
+		int N = sc.nextInt();
 
-		for (int i = 1; i <= n; i++) {
+		for (int i = 1; i <= N; i++) {
 			System.out.print("Tax payer #" + i + " data:");
 			System.out.print("individual or company (i/c)? ");
-			char ch = sc.next().charAt(0);
+			char type = sc.next().charAt(0);
 			System.out.print("Name: ");
 			sc.nextLine();
 			String name = sc.nextLine();
 			System.out.print("Anual income: ");
-			double income = sc.nextDouble();
-			if (ch == 'i' || ch == 'I') {
+			double anualIncome = sc.nextDouble();
+			if (type == 'i' || type == 'I') {
 				System.out.print("Health expenditures: ");
 				double healthExpenditures = sc.nextDouble();
-				list.add(new Individual(name, income, healthExpenditures));
-			} else if (ch == 'c' || ch == 'C') {
+				list.add(new Individual(name, anualIncome, healthExpenditures));
+			} else if (type == 'c' || type == 'C') {
 				System.out.print("Number of employees: ");
-				int numberOfEmployees = sc.nextInt();
-				list.add(new Company(name, income, numberOfEmployees));
+				Integer numberOfEmployees = sc.nextInt();
+				list.add(new Company(name, anualIncome, numberOfEmployees));
 			}
 		}
-
 		System.out.println();
-		System.out.println("TAXES PAID");
+		System.out.println("TAXES PAID:");
 
 		for (TaxPayer taxes : list) {
-			System.out.print(taxes.tax());
+			System.out.println(taxes.getName() + ": $ " + String.format("%.2f", taxes.tax()));
 		}
 
+		double sum = 0.0;
+		for (TaxPayer taxes : list) {
+			sum += taxes.tax();
+
+		}
+		System.out.println();
+		System.out.println("TOTAL TAXES: $ " + String.format("%.2f", sum));
 		sc.close();
 	}
 
