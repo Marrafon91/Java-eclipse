e1e4bbeeaa988fff8198473f8e59771349242c89
GuiM <Guilherme.Marrafon1@gmail.com>
2025-07-15 19:05:49 -0300

    Summary

--- PlanoSaude/src/application/Program.java
+++ PlanoSaude/src/application/Program.java
@@ -1,10 +1,32 @@
 package application;
 
+import java.util.Locale;
+import java.util.Scanner;
+
 public class Program {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		Locale.setDefault(Locale.US);
+		Scanner sc = new Scanner(System.in);
+		
+		System.out.print("Enter the number od tax payers: ");
+		int n = sc.nextInt();
+		
+		for (int i = 1; i <= n; i++) {
+			System.out.print("Tax payer #" + i + " data:");
+			System.out.print("individual or company (i/c)? ");
+			char ch = sc.next().charAt(0);
+			System.out.print("Name: ");
+			sc.nextLine();
+			String name = sc.nextLine();
+			System.out.print("Anual income: ");
+			double income = sc.nextDouble();
+			if(ch == 'i' || ch == 'I') {
+				
+			}
+		}
+		
+		sc.close();
 	}
 
 }
