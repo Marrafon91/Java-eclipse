5b1339fd40981e7be4efc444fb9a61e12102275a
GuiM <Guilherme.Marrafon1@gmail.com>
2025-07-15 08:30:41 -0300

    Summary

---
+++ Ex_Polimorfismo/src/application/Program.java
@@ -0,0 +1,38 @@
+package application;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Locale;
+import java.util.Scanner;
+
+import entities.Product;
+
+public class Program {
+
+	public static void main(String[] args) {
+		Locale.setDefault(Locale.US);
+		Scanner sc = new Scanner(System.in);
+
+		List<Product> list = new ArrayList<>();
+
+		System.out.print("Enter the number of products: ");
+		int n = sc.nextInt();
+
+		for (int i = 1; i <= n; i++) {
+			System.out.print("Product #" + i + ":");
+			System.out.print("Common, used or imported (c/u/i)? ");
+			char ch = sc.next().charAt(0);
+			System.out.print("Name: ");
+			sc.nextLine();
+			String name = sc.nextLine();
+			System.out.print("Price: ");
+			double price = sc.nextDouble();
+			if (ch == 'c' || ch == 'C') {
+
+			}
+		}
+
+		sc.close();
+	}
+
+}
