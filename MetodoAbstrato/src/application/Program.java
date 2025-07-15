801916e40ef3f59dda89bc587c1d97600f931f10
GuiM <Guilherme.Marrafon1@gmail.com>
2025-07-15 17:24:39 -0300

    Update

--- MetodoAbstrato/src/application/Program.java
+++ MetodoAbstrato/src/application/Program.java
@@ -1,16 +1,40 @@
 package application;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Locale;
 import java.util.Scanner;
+
+import entities.Shape;
+import entities.enums.Color;
 
 public class Program {
 
 	public static void main(String[] args) {
 		Locale.setDefault(Locale.US);
 		Scanner sc = new Scanner(System.in);
-		
-		
-		
+
+		List<Shape> list = new ArrayList<>();
+
+		System.out.print("Enter the number od shapes: ");
+		int n = sc.nextInt();
+
+		for (int i = 1; i <= n; i++) {
+			System.out.println("Shape #" + i + " data:");
+			System.out.print("Rectangle or Circle (r/c)? ");
+			char ch = sc.next().charAt(0);
+			System.out.print("(BLACK/BLUE/RED): ");
+			Color color = Color.valueOf(sc.next());
+			if (ch == 'r' || ch == 'R') {
+				System.out.print("Width: ");
+				double width = sc.nextDouble();
+				System.out.print("Height: ");
+				double height = sc.nextDouble();
+
+			}
+
+		}
+
 		sc.close();
 	}
 
