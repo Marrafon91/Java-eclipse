4716b75fa0d9a9b6bd425c61594bab505f9034f3
GuiM <Guilherme.Marrafon1@gmail.com>
2025-07-17 12:55:07 -0300

    aula028

---
+++ curso_programacao/src/curso_programacao/aula028try_catch.java
@@ -0,0 +1,27 @@
+package curso_programacao;
+
+import java.util.InputMismatchException;
+import java.util.Locale;
+import java.util.Scanner;
+
+public class aula028try_catch {
+
+	public static void main(String[] args) {
+		Locale.setDefault(Locale.US);
+		Scanner sc = new Scanner(System.in);
+
+		try {
+			String[] vect = sc.nextLine().split(" ");
+			int position = sc.nextInt();
+			System.out.println(vect[position]);
+		} catch (ArrayIndexOutOfBoundsException e) {
+			System.out.println("Invalid! position!");
+		} catch (InputMismatchException e) {
+			System.out.println("Input error!");
+		}
+
+		System.out.println("End of Program");
+		sc.close();
+	}
+
+}
