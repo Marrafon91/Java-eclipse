3ce9943d3a85169f3300e16a4388552cf32418c0
GuiM <Guilherme.Marrafon1@gmail.com>
2025-07-19 14:36:54 -0300

    bad solution

--- ReservaHotels/src/model/entities/Reservation.java
+++ ReservaHotels/src/model/entities/Reservation.java
@@ -12,9 +12,6 @@ public class Reservation {
 
 	private static SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy");
 
-	public Reservation() {
-	}
-
 	public Reservation(Integer roomNumber, Date checkin, Date checkout) {
 		this.roomNumber = roomNumber;
 		this.checkIn = checkin;
@@ -42,14 +39,27 @@ public class Reservation {
 		return TimeUnit.DAYS.convert(diff, TimeUnit.MILLISECONDS);
 	}
 
-	public void updateDates(Date checkIn, Date checkOut) {
+	public String updateDates(Date checkIn, Date checkOut) {
+		Date now = new Date();
+		if (checkIn.before(now) || checkOut.before(now)) {
+			return "Reservation dates for update must be future dates";
+		}if (!checkOut.after(checkIn)) {
+			return "Check-out date must be after Check-in date";
+		}		
 		this.checkIn = checkIn;
 		this.checkOut = checkOut;
+		return null;
 	}
 
 	@Override
 	public String toString() {
-		return "Room " + roomNumber + ", checkIn: " + sdf.format(checkIn) + ", checkOut: " + sdf.format(checkOut) + ", "
+		return "Room "
+				+ roomNumber 
+				+ ", checkIn: " 
+				+ sdf.format(checkIn) 
+				+ ", checkOut: " 
+				+ sdf.format(checkOut) 
+				+ ", "
 				+ duration() + " nights";
 	}
 }
