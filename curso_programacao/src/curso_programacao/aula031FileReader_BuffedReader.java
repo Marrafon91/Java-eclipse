d80dbb18eccaae1c3d349c7a1e233f33c1f3c435
GuiM <Guilherme.Marrafon1@gmail.com>
2025-07-20 19:43:41 -0300

    Update

--- curso_programacao/src/curso_programacao/aula031FileReader_BuffedReader.java
+++ curso_programacao/src/curso_programacao/aula031FileReader_BuffedReader.java
@@ -11,13 +11,8 @@ public class aula031FileReader_BuffedReader {
 		Locale.setDefault(Locale.US);
 
 		String path = "C:\\Users\\Guilherme\\Desktop\\PDF-logica de Programação\\aula.txt";
-		FileReader fr = null;
-		BufferedReader br = null;
 
-		try {
-			fr = new FileReader(path);
-			br = new BufferedReader(fr);
-
+		try (BufferedReader br = new BufferedReader(new FileReader(path))) {
 			String line = br.readLine();
 
 			while (line != null) {
@@ -26,18 +21,6 @@ public class aula031FileReader_BuffedReader {
 			}
 		} catch (IOException e) {
 			System.out.println("Error: " + e.getMessage());
-		} finally {
-			try {
-				if (br != null) {
-					br.close();
-				}
-				if (fr != null) {
-					fr.close();
-				}
-			} catch (IOException e) {
-				e.printStackTrace();
-			}
 		}
-
 	}
 }
