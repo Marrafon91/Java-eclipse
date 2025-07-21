562db20c91d0051e3eb6b320adeb69bb46478d4e
GuiM <Guilherme.Marrafon1@gmail.com>
2025-07-21 10:09:01 -0300

    Summary

---
+++ CarrRent/src/model/entities/CarrRental.java
@@ -0,0 +1,16 @@
+package model.entities;
+
+import java.time.LocalDateTime;
+
+public class CarrRental {
+	
+	private LocalDateTime start;
+	private LocalDateTime finish;
+	
+	public CarrRental() {
+	}
+	
+	
+	
+	
+}
