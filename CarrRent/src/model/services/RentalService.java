24bb27c739d513421a46a348ea1188e8e152b11a
GuiM <Guilherme.Marrafon1@gmail.com>
2025-07-21 16:15:10 -0300

    Summary

--- CarrRent/src/model/services/RentalService.java
+++ CarrRent/src/model/services/RentalService.java
@@ -6,12 +6,13 @@ import model.entities.CarrRental;
 import model.entities.Invoice;
 
 public class RentalService {
+
 	private Double pricePerHour;
 	private Double pricePerDay;
 
-	private BrazilTaxService taxService;
+	private TaxService taxService;
 
-	public RentalService(Double pricePerHour, Double pricePerDay, BrazilTaxService taxService) {
+	public RentalService(Double pricePerHour, Double pricePerDay, TaxService taxService) {
 		this.pricePerHour = pricePerHour;
 		this.pricePerDay = pricePerDay;
 		this.taxService = taxService;
