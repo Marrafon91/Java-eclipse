877f2187148a5bbc847d2f305333781a30907df9
GuiM <Guilherme.Marrafon1@gmail.com>
2025-07-21 10:19:59 -0300

    Summary

---
+++ CarrRent/src/application/Program.java
@@ -0,0 +1,27 @@
+package application;
+
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+import java.util.Locale;
+import java.util.Scanner;
+
+public class Program {
+
+	public static void main(String[] args) {
+		Locale.setDefault(Locale.US);
+		Scanner sc = new Scanner(System.in);
+
+		DateTimeFormatter fmt = DateTimeFormatter.ofPattern("dd/MM/yyy HH:mm");
+
+		System.out.println("Entre com os dados do aluguel: ");
+		System.out.print("Modelo do carro: ");
+		String carrModel = sc.nextLine();
+		System.out.print("Retirada (dd/MM/yyyy): ");
+		LocalDateTime start = LocalDateTime.parse(sc.nextLine(), fmt);
+		System.out.print("Retorno (dd/MM/yyyy): ");
+		LocalDateTime finish = LocalDateTime.parse(sc.nextLine(), fmt);
+
+		sc.close();
+	}
+
+}
