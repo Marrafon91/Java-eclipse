751945e88e31f29b1963cc5f1bf10aad6a7209d6
GuiM <Guilherme.Marrafon1@gmail.com>
2025-07-23 11:37:06 -0300

    Summary

---
+++ curso_programacao/src/curso_programacao/aula035HerdarVsContrato.java
@@ -0,0 +1,20 @@
+package curso_programacao;
+
+import java.util.Locale;
+import java.util.Scanner;
+
+public class aula035HerdarVsContrato  { // HERANÇA E INTERFACES.
+
+	public static void main(String[] args) {
+		Locale.setDefault(Locale.US);
+		Scanner sc = new Scanner(System.in);
+		// RELAÇÃO É-UM.
+		// GENERALIZAÇÃO/ESPECIALIZAÇÃO.
+		// POLIMORFISMO.
+		// HERANÇA => REUSO.
+		// INTERFACES => CONTRATO A SER CUMPRIDO.
+		
+		sc.close();
+	}
+
+}
