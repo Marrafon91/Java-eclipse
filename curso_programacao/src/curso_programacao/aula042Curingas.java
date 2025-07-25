7e2917e04ee9cba50c3039e3f3c3758b65a044a7
GuiM <Guilherme.Marrafon1@gmail.com>
2025-07-25 17:49:53 -0300

    Update aula042

--- curso_programacao/src/curso_programacao/aula042Curingas.java
+++ curso_programacao/src/curso_programacao/aula042Curingas.java
@@ -1,6 +1,7 @@
 package curso_programacao;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 import entities.Circle;
@@ -15,16 +16,43 @@ public class aula042Curingas {
 		myShapes.add(new Rectangle(3.0, 2.0));
 		myShapes.add(new Circle(2.0));
 
-		System.out.println("Total area: " + totalArea(myShapes));
+		List<Circle> myCircle = new ArrayList<Circle>();
+		myCircle.add(new Circle(2.0));
+
+		System.out.println("Total area Rectangle: " + totalArea(myShapes));
+		System.out.println("Total area Circle: " + totalArea(myCircle));
+
+		List<Integer> myInts = Arrays.asList(1, 2, 3, 4);
+		List<Double> myDouble = Arrays.asList(3.14, 6.28);
+		List<Object> myObjs = new ArrayList<Object>();
+
+		copy(myInts, myObjs);
+		printList(myObjs);
+		copy(myDouble, myObjs);
+		printList(myObjs);
 
 	}
 
-	public static String totalArea(List<Shape> list) {
+	public static void copy(List<? extends Number> source, List<? super Number> destiny) {
+		for (Number number : source) {
+			destiny.add(number);
+		}
+	}
+	
+	public static void printList(List<?> list) {
+		for (Object obj : list) {
+			System.out.print(obj + ", ");
+		}
+		System.out.println();
+	}
+
+	public static String totalArea(List<? extends Shape> list) {
 		double sum = 0.0;
 		for (Shape s : list) {
 			sum += s.area();
 		}
 		return String.format("%.2f", sum);
+		// Nao consegue add elementos a List
 	}
 
 }
