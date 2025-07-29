b0a5958b97951c961555ce4c1848ad1fc9509314
GuiM <Guilherme.Marrafon1@gmail.com>
2025-07-29 11:30:54 -0300

    aula046

---
+++ curso_programacao/src/curso_programacao/aula046.java
@@ -0,0 +1,34 @@
+package curso_programacao;
+
+import java.util.Map;
+import java.util.TreeMap;
+
+public class aula046 {
+
+	public static void main(String[] args) {
+				
+		Map<String, String> cookies = new TreeMap<>();
+		
+		cookies.put("username", "Maria");
+		cookies.put("email", "maria@gmail.com");
+		cookies.put("phone", "9971122");
+		
+		cookies.remove("email");
+		cookies.put("phone", "99771133");
+		
+		System.out.println("Contains 'phone' key: " + cookies.containsKey("phone"));
+		System.out.println("Phone number: " + cookies.get("phone"));
+		System.out.println("Email: " + cookies.get("email"));
+		System.out.println("Size: " + cookies.size());
+		
+		
+		System.out.println("ALL COOKIES: ");
+		
+		for(String key : cookies.keySet()) {
+			System.out.println(key + ": " + cookies.get(key));
+		}
+		
+		System.out.println("#####################\n");
+		
+	}
+}
