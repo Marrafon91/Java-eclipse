4c8ec452d0b9f4c607cec613c599b2922d3a4ba1
Guilherme Marrafon <Guilherme.Marrafon1@gmail.com>
2025-08-12 07:42:00 -0300

    Create database

--- aulaJPA/src/dominio/Pessoa.java
+++ aulaJPA/src/dominio/Pessoa.java
@@ -1,10 +1,60 @@
 package dominio;
 
-public class Pessoa {
+import java.io.Serializable;
 
-	public static void main(String[] args) {
-		System.out.println("Hello world!!!");
+public class Pessoa implements Serializable {
 
+	private static final long serialVersionUID = 1L;
+
+	private Integer id;
+	private String name;
+	private String email;
+
+	public Pessoa() {
+	}
+
+	public Pessoa(Integer id, String name, String email) {
+		super();
+		this.id = id;
+		this.name = name;
+		this.email = email;
+	}
+
+	public Integer getId() {
+		return id;
+	}
+
+	public void setId(Integer id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	@Override
+	public String toString() {
+		StringBuilder builder = new StringBuilder();
+		builder.append("Pessoa [id=");
+		builder.append(id);
+		builder.append(", name=");
+		builder.append(name);
+		builder.append(", email=");
+		builder.append(email);
+		builder.append("]");
+		return builder.toString();
 	}
 
 }
